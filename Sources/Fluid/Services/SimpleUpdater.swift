3bc418acc81155729202f72b922e1c8dc8a3afd6
sfasano <sfasano@gmail.com>
2026-08-05 13:34:23 -0400

    Fix multiple instances remaining after update

--- Sources/Fluid/Services/SimpleUpdater.swift
+++ Sources/Fluid/Services/SimpleUpdater.swift
@@ -918,7 +918,7 @@ final class SimpleUpdater {
                 DebugLogger.shared.info("SimpleUpdater: Successfully relaunched app, terminating old instance", source: "SimpleUpdater")
                 // Give the new instance time to fully start before terminating
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                    NSApp.terminate(nil)
+                    exit(0)
                 }
             }
         }
